3ac701273fbd8388962782129449ffbd30560f35
NahoBEEHO <1517659436@qq.com>
2025-09-12 23:59:07 +0800

    add some changes

--- web/src/api/facilities.js
+++ web/src/api/facilities.js
@@ -1,26 +1,63 @@
-import axios from 'axios';
+import request from '@/utils/request'
 
-export const createRide = async (data) => {
-  const response = await axios.post('/api/resource/rides', data);
-  return response.data;
-};
+// 游乐设施管理 API - 基于后端 /api/resource/rides 路径
 
-export const updateRide = async (id, data) => {
-  const response = await axios.put(`/api/resource/rides/${id}`, data);
-  return response.data;
-};
+/**
+ * 创建游乐设施
+ * @param {Object} data 设施数据
+ */
+export function createRide(data) {
+  return request({
+    url: '/api/resource/rides',
+    method: 'post',
+    data,
+  })
+}
 
-export const deleteRide = async (id) => {
-  const response = await axios.delete(`/api/resource/rides/${id}`);
-  return response.data;
-};
+/**
+ * 更新游乐设施
+ * @param {string|number} id 设施ID
+ * @param {Object} data 更新数据
+ */
+export function updateRide(id, data) {
+  return request({
+    url: `/api/resource/rides/${id}`,
+    method: 'put',
+    data,
+  })
+}
 
-export const searchRides = async (params) => {
-  const response = await axios.get('/api/resource/rides/search', { params });
-  return response.data;
-};
+/**
+ * 删除游乐设施
+ * @param {string|number} id 设施ID
+ */
+export function deleteRide(id) {
+  return request({
+    url: `/api/resource/rides/${id}`,
+    method: 'delete',
+  })
+}
 
-export const getRideStats = async (params) => {
-  const response = await axios.get('/api/resource/rides/stats', { params });
-  return response.data;
-};
+/**
+ * 搜索游乐设施，支持多条件筛选和分页
+ * @param {Object} params 搜索参数
+ */
+export function searchRides(params) {
+  return request({
+    url: '/api/resource/rides/search',
+    method: 'get',
+    params,
+  })
+}
+
+/**
+ * 获取游乐设施统计数据
+ * @param {Object} params 统计参数
+ */
+export function getRideStats(params) {
+  return request({
+    url: '/api/resource/rides/stats',
+    method: 'get',
+    params,
+  })
+}
